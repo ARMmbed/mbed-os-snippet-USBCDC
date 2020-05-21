829975aa0e1b1ed25b183ef671e60244e58984e8
adbridge <anna.bridge@arm.com>
2020-05-21 17:06:50 +0100

    Mirrored from master snippet repository

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2006-2020 Arm Limited and affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include "mbed.h"
+#include "USBCDC.h"
+
+USBCDC cdc;
+
+int main(void)
+{
+
+    while (1) {
+        char msg[] = "Hello world\r\n";
+        cdc.send((uint8_t *)msg, strlen(msg));
+        ThisThread::sleep_for(1000);
+    }
+}
